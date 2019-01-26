2fbc1dde29cd813e7c758185af6e69f944534d7d
Riley Norris <rileydnorris@gmail.com>
2019-01-26 12:46:19 -0600

    Added pods

---
+++ OpenInvite/Pods/Target Support Files/MaterialComponents/MaterialComponents-umbrella.h
@@ -0,0 +1,51 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CAMediaTimingFunction+MDCAnimationTiming.h"
+#import "MaterialAnimationTiming.h"
+#import "UIView+MDCTimingFunction.h"
+#import "MaterialPalettes.h"
+#import "MDCPalettes.h"
+#import "MaterialTextFields.h"
+#import "MDCIntrinsicHeightTextView.h"
+#import "MDCMultilineTextField.h"
+#import "MDCMultilineTextInputDelegate.h"
+#import "MDCTextField.h"
+#import "MDCTextFieldPositioningDelegate.h"
+#import "MDCTextInput.h"
+#import "MDCTextInputBorderView.h"
+#import "MDCTextInputCharacterCounter.h"
+#import "MDCTextInputController.h"
+#import "MDCTextInputControllerBase.h"
+#import "MDCTextInputControllerFilled.h"
+#import "MDCTextInputControllerFloatingPlaceholder.h"
+#import "MDCTextInputControllerFullWidth.h"
+#import "MDCTextInputControllerLegacyDefault.h"
+#import "MDCTextInputControllerLegacyFullWidth.h"
+#import "MDCTextInputControllerOutlined.h"
+#import "MDCTextInputControllerOutlinedTextArea.h"
+#import "MDCTextInputControllerUnderline.h"
+#import "MDCTextInputUnderlineView.h"
+#import "MaterialTypography.h"
+#import "MDCFontTextStyle.h"
+#import "MDCTypography.h"
+#import "UIFont+MaterialSimpleEquality.h"
+#import "UIFont+MaterialTypography.h"
+#import "UIFontDescriptor+MaterialTypography.h"
+#import "MaterialApplication.h"
+#import "UIApplication+AppExtensions.h"
+#import "MaterialMath.h"
+#import "MDCMath.h"
+
+FOUNDATION_EXPORT double MaterialComponentsVersionNumber;
+FOUNDATION_EXPORT const unsigned char MaterialComponentsVersionString[];
+
---
+++ OpenInvite/Pods/Target Support Files/Pods-OpenInviteUITests/Pods-OpenInviteUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OpenInviteUITests : NSObject
+@end
+@implementation PodsDummy_Pods_OpenInviteUITests
+@end
