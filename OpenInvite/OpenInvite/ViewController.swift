e61ae5175a103b52381e6fd547e13eb4705a738a
Riley Norris <rileydnorris@gmail.com>
2019-01-26 12:33:43 -0600

    Initial commit

---
+++ OpenInvite/OpenInvite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenInvite
+//
+//  Created by Riley Norris on 1/26/19.
+//  Copyright © 2019 Riley Norris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
