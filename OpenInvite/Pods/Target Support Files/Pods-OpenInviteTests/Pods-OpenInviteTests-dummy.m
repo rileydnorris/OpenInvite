ea1ffee38bceab3af09fee2a5ab26811860634cb
Riley Norris <rileydnorris@gmail.com>
2019-01-26 17:36:50 -0600

    Fixed tracking

--- OpenInvite/Pods/Target Support Files/Pods-OpenInviteTests/Pods-OpenInviteTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OpenInviteTests : NSObject
-@end
-@implementation PodsDummy_Pods_OpenInviteTests
-@end
--- OpenInvite/Pods/Target Support Files/Pods-OpenInviteUITests/Pods-OpenInviteUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OpenInviteUITests : NSObject
-@end
-@implementation PodsDummy_Pods_OpenInviteUITests
-@end
