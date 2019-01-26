46b9e0facf37ef3e18ba0e8e4d572c78e8d405e4
Riley Norris <rileydnorris@gmail.com>
2019-01-26 14:53:22 -0600

    Added snapchat authentication

--- OpenInvite/OpenInvite/Utilities/AppDelegate.swift
+++ OpenInvite/OpenInvite/Utilities/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import SCSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -42,6 +43,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
         self.saveContext()
+    }
+    
+    /// Handling redirect of snapchat's login
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if SCSDKLoginClient.application(app, open: url, options: options) {
+            return true
+        }
+        return true
     }
 
     // MARK: - Core Data stack
--- OpenInvite/OpenInvite/ViewController.swift
+++ OpenInvite/OpenInvite/ViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+import SCSDKLoginKit
 
 class ViewController: UIViewController {
-
+    
+    /// Action to login a user via Snapchat's API
+    @IBAction func loginAction(_ sender: Any) {
+        SCSDKLoginClient.login(from: self) { success, error in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print(success)
+            }
+        }
+    }
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
